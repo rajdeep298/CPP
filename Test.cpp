7d84e8e7ed2b8740ae78d9e06db5a4d2430c3bc5
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-09-24 10:35:37 +0530

    Some mild changes

--- Test.cpp
+++ Test.cpp
@@ -2,28 +2,71 @@ using namespace std;
 
 #include <iostream>
 
-class complex{
+class Complex{
     private:
         float real=0, imaginary=0;
     public:
-        complex(float x, float y){
-            this->real = x;
-            this->imaginary = y;
-        }
-        complex add(complex &c1){//operator overloading
-            complex add(0,0);
+        Complex(){
+            real = 0;
+            imaginary = 0;
+        };
+        Complex(float x, float y){
+            real = x;
+            imaginary = y;
+        };
+        Complex operator+(Complex &c1){//operator overloading
+            Complex add;
             add.real = this->real + c1.real;
             add.imaginary = this->imaginary + c1.imaginary;
             return add;
         }
-        complex multiply(complex &c1){//operator overloading
-            complex multiply(0,0);
+        Complex operator*(Complex &c1){//operator overloading
+            Complex multiply;
             multiply.real = this->real*c1.real - this->imaginary*c1.imaginary;
             multiply.imaginary = this->real*c1.imaginary + this->imaginary*c1.real;
             return multiply;
         }
+        // ostream& operator<<(ostream &out, const Complex &c1) {//operator overloading
+        //     out << c1.real << "+ i." << c1.imaginary<<endl; 
+        //     return out;
+        // }
+        friend Complex operator-(Complex &c1,Complex &c2){//friend function
+            Complex subtract;
+            subtract.real = c1.real - c2.real;
+            subtract.imaginary = c1.imaginary - c2.imaginary;
+            return subtract;
+        }
+        friend class Print;
+};
+
+class Print{//friend class
+    public:
+        void print(Complex &c1){
+            cout <<"Addition: "<< c1.real << "+ i." << c1.imaginary<<endl;
+        }
 };
 
 int main(){
+    int r1,i1,r2,i2;
+    cout << "Enter real and imaginary part of first complex number: ";
+    cin >> r1 >> i1;
+    cout << "Enter real and imaginary part of second complex number: ";
+    cin >> r2 >> i2; 
+
+
+    Complex c1(r1,i1),c2(r2,i2);
+    Print p1;
+    Complex addition=c1 + c2;
+    Complex multiplication=c1*c2 ;
+    Complex subtraction=c1-c2;
+    // cout << addition << "," << multiplication << "," << subtraction << endl ;
+    
+    cout<<"Addition:";
+    p1.print(addition);
+    cout<<"Multiplication:";
+    p1.print(multiplication);
+    cout<<"Subtraction:";   
+    p1.print(subtraction);
+
     return 0;
 }
