b36028cf58f10c99e61883d42f66c8d4716167ac
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-04-26 20:56:48 +0530

    some mild changes

---
+++ 5-Template_Function.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+
+template <typename T>
+T add(T a, T b){
+    return a + b;
+}
+
+int main(){
+    cout << add<int>(1, 2) << endl;
+    cout << add<double>(1.1, 2.2) << endl;
+    cout << add<string>("Hello", "World") << endl;
+}
