d2d8412c8fada51e4550a6e1e627dacb077ba628
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-04-28 22:10:51 +0530

    some mild chnages

---
+++ 6-NameSpace_Nested.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include <iostream>
+
+namespace namespaceOuter{
+    void func(){
+        cout << "namespace1::func()" << endl;
+    }
+    namespace namespaceInner{
+        void func(){
+            cout << "namespaceInner::func()" << endl;
+        }
+    }
+}
+
+using namespace namespaceOuter;
+int main(){
+    func();//as we have used using namespace namespace1, so it will call namespace1::func()
+    namespaceInner::func();// this will call namespace2::func(). If we remove using namespace namespace1, then it will give error as func() is ambigious
+}
