6f3ce31e893ad664df2cbc5b5f5737fd2e24501a
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-05-02 08:24:17 +0530

    some mild changes

---
+++ 11-Const_Member_Functions.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+#include <iostream>
+
+class Update{
+    private:
+        int x;
+
+    public:
+        Update(int x) : x(x) {} // initializer list
+        void updateX() const{
+            x++; // this line will give error as this function is  constant
+            cout << "Value of x is: "<<x<<endl;
+        }
+        void updateX(int x) const{
+            this->x=x; // this line will give error as this pointer is not allowed in constant member function
+            cout << "Value of x is: "<<x<<endl;
+        }
+};
+
+int main(){
+    Update u(10);
+    u.updateX();
+    u.updateX(10);
+    return 0;
+}
