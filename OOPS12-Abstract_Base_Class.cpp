7df464c39f566cc3c0c1a8d5dcc9fcdf2a34e7b0
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-05-03 02:50:06 +0530

    some mild changes

---
+++ OOPS12-Abstract_Base_Class.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include <iostream>
+
+class abstractBaseClass {
+public:
+  virtual void sayHello() = 0; // Pure virtual function
+};
+
+class derivedClass: public abstractBaseClass {
+public:
+  void sayHello() {
+    cout << "Hello! I am a derived class!" << endl;
+  }
+};
+
+int main() {
+  derivedClass anObject = derivedClass();
+  anObject.sayHello();
+}
