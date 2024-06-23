7cad921a9f86aff3c5334dc63446d22968274422
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-06-24 00:12:10 +0530

    some mild changes

--- OOPS1-Addition.cpp
+++ OOPS1-Addition.cpp
@@ -8,6 +8,10 @@ class Calculate{
             return a+b;
         }
 };
+Calculate::Calculate() {//external definition of this constructor
+    a=10;
+    b=20;
+}
 int main() {
     Calculate c;
     int sum=c.add(10,20);
