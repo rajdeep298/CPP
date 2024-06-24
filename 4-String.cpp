5115d0f0092cdd8ed5ebe433310da48b639a4897
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-06-24 21:13:49 +0530

    some mild changes

--- 4-String.cpp
+++ 4-String.cpp
@@ -17,5 +17,18 @@ int main(){
     // while(ss >> word){
     //     cout << word << endl;
     // }
+
+    // char *ptr ="Rajdeep Kundu"; //C++ forbids converting a string constant to char*
+    // cout<<*ptr;
+
+    //Instead we can do this 
+    string s ="Rajdeep Kundu";
+    char *ptr =&s[0];
+    cout<<*ptr;
+
+    //or
+    char array[]="Rajdeep Kundu";
+    char *ptr1 =array;
+    cout<<*ptr1;
     
 }
